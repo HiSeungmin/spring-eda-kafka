b185417ae51f08f561dbbbdaea4bd86f138941d0
HiSeungmin <osminomsin7180@gmail.com>
2026-05-19 10:19:22 +0900

    setting project

---
+++ src/test/java/com/sminoh/spring_eda_kafka/SpringEdaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sminoh.spring_eda_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEdaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
