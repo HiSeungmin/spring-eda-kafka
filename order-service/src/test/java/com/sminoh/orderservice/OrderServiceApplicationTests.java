18db5f61c02f5caa1df8c0cd7081f19b0fcc2679
HiSeungmin <osminomsin7180@gmail.com>
2026-05-22 17:38:12 +0900

    set project structure

--- order-service/src/test/java/com/sminoh/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/sminoh/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sminoh.spring_eda_kafka;
+package com.sminoh.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringEdaKafkaApplicationTests {
+class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
